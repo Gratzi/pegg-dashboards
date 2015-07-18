0af1f30fc1c32a633f16774c128b8b1846b2d32d
Auggernaut <augustin@datacosmos.com>
2015-07-17 17:15:50 -0700

    add Average Session, Game, and Section time

--- pegg.dashboard.js
+++ pegg.dashboard.js
@@ -43,15 +43,16 @@ Keen.ready(function(){
     timezone: "UTC"
   });
   client.draw(averageLoadTime, document.getElementById("chart-06"), {
-    title: "Average Load Time (s)"
+    title: "Average Load Time (ms)"
   });
 
 
   // ----------------------------------------
-  // Page Popularity Pie Chart
+  // Mood Popularity Pie Chart
   // ----------------------------------------
   var pagePoplarity = new Keen.Query("count", {
-    eventCollection: "showPage",
+    eventCollection: "userAction",
+    filters: [{"operator":"contains","property_name":"route","property_value":"/mood/"}],
     groupBy: "route",
     timezone: "UTC"
   });
@@ -120,13 +121,16 @@ Keen.ready(function(){
   //// Daily Active Users
   //// ----------------------------------------
   var query = new Keen.Query("count_unique", {
-    eventCollection: "showPage",
+    eventCollection: "userAction",
+    filters: [{"operator":"exists","property_name":"userId","property_value":true}],
     interval: "daily",
     targetProperty: "userId",
     timeframe: "this_7_days",
     timezone: "UTC"
   });
   client.draw(query, document.getElementById("chart-05"), {
+    height: 250,
+    width: "auto",
     chartOptions: {
       chartArea: {
         height: "85%",
@@ -141,8 +145,9 @@ Keen.ready(function(){
   //// Weekly Active Users
   //// ----------------------------------------
   var query = new Keen.Query("count_unique", {
-    eventCollection: "showPage",
+    eventCollection: "userAction",
     targetProperty: "userId",
+    filters: [{"operator":"exists","property_name":"userId","property_value":true}],
     timeframe: "this_7_days",
     timezone: "UTC"
   });
@@ -150,5 +155,172 @@ Keen.ready(function(){
     title: "Weekly Active Users"
   });
 
+  //// ----------------------------------------
+  //// Section loads
+  //// ----------------------------------------
+  var query = new Keen.Query("count", {
+    eventCollection: "userAction",
+    groupBy: "page",
+    timezone: "UTC"
+  });
+  client.draw(query, document.getElementById("chart-08"), {
+    chartType: "piechart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "100%"
+      },
+      pieHole: .2
+    }
+  });
+
+
+
+  //// ----------------------------------------
+  //// Session lengths
+  //// ----------------------------------------
+  var sessionLength = new Keen.Query("maximum", {
+    eventCollection: "userAction",
+    groupBy: "sessionId",
+    targetProperty: "sessionLength",
+    timeframe: "this_7_days",
+    timezone: "UTC"
+  });
+  client.draw(sessionLength, document.getElementById("chart-09"), {
+    chartType: "columnchart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      legend: {position: "none"},
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "100%"
+      },
+      isStacked: false
+    }
+  });
+
+  //// ----------------------------------------
+  //// Average Session length
+  //// ----------------------------------------
+  var sessionMetric = new Keen.Dataviz()
+    .el(document.getElementById("chart-11"))
+    .chartType("metric")
+    .prepare(); // start spinner
+
+  var req = client.run(sessionLength, function(err, res){
+    if (err) {
+      // Display the API error
+      sessionMetric.error(err.message);
+    }
+    else {
+      var sessionTimes = res.result;
+      var totalTime = 0
+      // Handle the response
+      for(var i=0; i<sessionTimes.length; i++){
+        totalTime += sessionTimes[i].result
+      }
+
+      sessionMetric
+        //.parseRequest(this)
+        .title("Average Session Length (s)")
+        .data({result: totalTime/sessionTimes.length})
+        .render();
+    }
+  });
+
+
+  //// ----------------------------------------
+  //// Time spent in each section
+  //// ----------------------------------------
+  var query = new Keen.Query("sum", {
+    eventCollection: "userAction",
+    filters: [{"operator":"exists","property_name":"lastEvent.page","property_value":true}],
+    groupBy: "lastEvent.page",
+    targetProperty: "lastEvent.eventLength",
+    timezone: "UTC"
+  });
+  client.draw(query, document.getElementById("chart-10"), {
+    chartType: "piechart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "100%"
+      },
+      pieHole: .2
+    }
+  });
+
+
+  //// ----------------------------------------
+  //// Weekly GameTime by User this week
+  //// ----------------------------------------
+  var weeklyGameTime = new Keen.Query("maximum", {
+    eventCollection: "userAction",
+    filters: [{"operator":"exists","property_name":"userId","property_value":true}],
+    groupBy: "userId",
+    targetProperty: "sessionLength",
+    timeframe: "this_7_days",
+    timezone: "UTC"
+  });
+  client.draw(weeklyGameTime, document.getElementById("chart-12"), {
+    chartType: "columnchart",
+    title: false,
+    height: 250,
+    width: "auto",
+    chartOptions: {
+      legend: {position: "none"},
+      chartArea: {
+        height: "85%",
+        left: "5%",
+        top: "5%",
+        width: "100%"
+      },
+      isStacked: false
+    }
+  });
+
+
+  //// ----------------------------------------
+  //// Average GameTime this week
+  //// ----------------------------------------
+  var gameTimeMetric = new Keen.Dataviz()
+    .el(document.getElementById("chart-13"))
+    .chartType("metric")
+    .prepare(); // start spinner
+
+  var req = client.run(weeklyGameTime, function(err, res){
+    if (err) {
+      // Display the API error
+      gameTimeMetric.error(err.message);
+    }
+    else {
+      var gameTimes = res.result;
+      var totalTime = 0
+      // Handle the response
+      for(var i=0; i<gameTimes.length; i++){
+        totalTime += gameTimes[i].result
+      }
+
+      gameTimeMetric
+        //.parseRequest(this)
+        .title("Average Game Time (s)")
+        .data({result: totalTime/gameTimes.length})
+        .render();
+    }
+  });
 
 });
